ae53e0ebe4a6ceb49bfdaaed7c0d8f2037b073d0
Ming Xia <mxia@linkedin.com>
2016-06-26 10:17:13 -0700

    temp version

--- SimpleStocks/src/main/java/com/kevinchou/simplestocks/SinaFinanceInfo.java
+++ SimpleStocks/src/main/java/com/kevinchou/simplestocks/SinaFinanceInfo.java
@@ -7,91 +7,133 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import org.junit.Test;
 
 
 /**
  * Created by mxia on 6/25/16.
  */
 public class SinaFinanceInfo {
-  // Yahoo finance symbols
-  final static String yPrice = "l1";
-  final static String yChange = "c";
-  final static String yPrevClose = "p";
-  final static String yOpen = "o";
-  final static String yBid = "b3";
-  final static String yAsk = "b2";
-  final static String yOneYrTarget = "t8";
-  final static String yMarketCap = "j1";
-  final static String yTrailingPE = "r";
-  final static String yForwardPE = "r7";
-  final static String yPegRatio = "r5";
-  final static String yPriceSales = "p5";
-  final static String yPriceBook = "p6";
-  final static String yEbitda = "j4";
-  final static String yTradeDate = "d1";
-  final static String yTradeTime = "t1";
-  final static String yExchange = "x";
-  final static String yName = "n";
-  final static String yDaysRange = "m0";
-  final static String yYearRange = "w0";
-  final static String yAvgDailyVolume = "a2";
-  final static String yVolume = "v0";
+  /**
+   *  Items in response. 顺序号从0开始。
+   0：”大秦铁路”，股票名字；
+   1：”27.55″，今日开盘价；
+   2：”27.25″，昨日收盘价；
+   3：”26.91″，当前价格；
+   4：”27.55″，今日最高价；
+   5：”26.20″，今日最低价；
+   6：”26.91″，竞买价，即“买一”报价；
+   7：”26.92″，竞卖价，即“卖一”报价；
+   8：”22114263″，成交的股票数，由于股票交易以一百股为基本单位，所以在使用时，通常把该值除以一百；
+   9：”589824680″，成交金额，单位为“元”，为了一目了然，通常以“万元”为成交金额的单位，所以通常把该值除以一万；
+   10：”4695″，“买一”申请4695股，即47手；
+   11：”26.91″，“买一”报价；
+   12：”57590″，“买二”
+   13：”26.90″，“买二”
+   14：”14700″，“买三”
+   15：”26.89″，“买三”
+   16：”14300″，“买四”
+   17：”26.88″，“买四”
+   18：”15100″，“买五”
+   19：”26.87″，“买五”
+   20：”3100″，“卖一”申报3100股，即31手；
+   21：”26.92″，“卖一”报价
+   (22, 23), (24, 25), (26,27), (28, 29)分别为“卖二”至“卖四的情况”
+   30：”2008-01-11″，日期；
+   31：”15:05:32″，时间；
+   */
+  final static int STOCK_NAME_INDEX = 0;
+  final static int OPEN_PRICE_INDEX = 1;
+  final static int PREV_CLOSE_INDEX = 2;
+  final static int CUR_PRICE_INDEX = 3;
+  final static int DAY_HIGH_INDEX = 4;
+  final static int DAY_LOW_INDEX = 5;
+  final static int BID_1_INDEX = 6;
+  final static int ASK_1_INDEX = 7;
+  final static int VOLUME_INDEX = 8;
+  final static int TRANSACTION_AMOUNT_INDEX = 9;
+  final static int BID_1_VOLUME_INDEX = 10;
+  final static int BID_1_PRICE_INDEX = 11;
+  final static int BID_2_VOLUME_INDEX = 12;
+  final static int BID_2_PRICE_INDEX = 13;
+  final static int BID_3_VOLUME_INDEX = 14;
+  final static int BID_3_PRICE_INDEX = 15;
+  final static int BID_4_VOLUME_INDEX = 16;
+  final static int BID_4_PRICE_INDEX = 17;
+  final static int BID_5_VOLUME_INDEX = 18;
+  final static int BID_5_PRICE_INDEX = 19;
+  final static int ASK_1_VOLUME_INDEX = 20;
+  final static int ASK_1_PRICE_INDEX = 21;
+  final static int ASK_2_VOLUME_INDEX = 22;
+  final static int ASK_2_PRICE_INDEX = 23;
+  final static int ASK_3_VOLUME_INDEX = 24;
+  final static int ASK_3_PRICE_INDEX = 25;
+  final static int ASK_4_VOLUME_INDEX = 26;
+  final static int ASK_4_PRICE_INDEX = 27;
+  final static int ASK_5_VOLUME_INDEX = 28;
+  final static int ASK_5_PRICE_INDEX = 29;
+  final static int DATE_INDEX = 30;
+  final static int TIME_INDEX = 31;
 
-  public static Stock getStockData(String ticker) {
-
-    String[] companyData;
-
-    Stock stock = new Stock();
-    stock.setTicker(ticker);
-
+  public static SinaStock getStockData(String tickerNumber) {
+    String[] responseData;
+    SinaStock sinaStock = new SinaStock();
     try {
-      URL url = new URL(
-          "http://finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=" + yPrice + yChange + yPrevClose + yOpen + yBid
-              + yAsk + yOneYrTarget + yMarketCap + yTrailingPE + yForwardPE + yPegRatio + yPriceSales + yPriceBook
-              + yEbitda + yTradeDate + yTradeTime + yExchange + yAvgDailyVolume + yVolume + yDaysRange + yYearRange
-              + yName);
-      URLConnection yc = url.openConnection();
-      BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-
-// Split CSV by comma and quotes
-      companyData = in.readLine().split(",");
-      in.close();
-
-// Convert price string to float, then round to 2 digits.
-      stock.setPrice(String.format("$%.2f", Float.parseFloat(companyData[0])));
-
-// Separate actual change and percent change
-      String[] priceChange = companyData[1].replaceAll("\"", "").split(" - ");
-      stock.setPriceChange(priceChange[0] + " (" + priceChange[1] + ")");
-
-      stock.setPrevClose(companyData[2]);
-      stock.setOpen(companyData[3]);
-      stock.setBid(companyData[4]);
-      stock.setAsk(companyData[5]);
-      stock.setOneYearTarget(companyData[6]);
-      stock.setMarketCap(companyData[7]);
-      stock.setTrailingPE(companyData[8]);
-      stock.setForwardPE(companyData[9]);
-      stock.setPegRatio(companyData[10]);
-      stock.setPriceSales(companyData[11]);
-      stock.setPriceBook(companyData[12]);
-      stock.setEbitda(companyData[13]);
-      stock.setTradeDate(companyData[14].replaceAll("\"", ""));
-      stock.setTradeTime(companyData[15].replaceAll("\"", ""));
-      stock.setExchange(companyData[16].replaceAll("\"", ""));
-      stock.setAvgDailyVolume(companyData[17]);
-      stock.setVolume(companyData[18]);
-      stock.setDaysRange(companyData[19].replaceAll("\"", ""));
-      stock.setYearRange(companyData[20].replaceAll("\"", ""));
-
-      // Name is always last, since name can contain a comma.
-      stock.setName(companyData[companyData.length - 1].replaceAll("\"", ""));
-    } catch (Exception e) {
-
-      Log.d("ERROR_GETTINGDATA", e.toString());
+      sinaStock.setTicketNumber(Integer.valueOf(tickerNumber));
+    } catch (NumberFormatException e) {
+      Log.e("TAG", "getStockData: ");
+      return null;
     }
 
-    return stock;
+    try {
+      URL url = new URL("http://hq.sinajs.cn/list=sh" + tickerNumber);
+      URLConnection urlConnection = url.openConnection();
+      BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+      // Split CSV by comma and quotes
+      responseData = in.readLine().split(",");
+      in.close();
+
+      sinaStock.setStockName(recover(responseData[STOCK_NAME_INDEX]));
+      sinaStock.setOpenPrice(responseData[OPEN_PRICE_INDEX]);
+      sinaStock.setPreCosePrice(responseData[PREV_CLOSE_INDEX]);
+      sinaStock.setCurPrice(responseData[CUR_PRICE_INDEX]);
+      sinaStock.setDayHigh(responseData[DAY_HIGH_INDEX]);
+      sinaStock.setDayLow(responseData[DAY_LOW_INDEX]);
+      sinaStock.setBid1(responseData[BID_1_INDEX]);
+      sinaStock.setAsk1(responseData[ASK_1_INDEX]);
+      sinaStock.setVolume(responseData[VOLUME_INDEX]);
+      sinaStock.setTransactionAmount(responseData[TRANSACTION_AMOUNT_INDEX]);
+      sinaStock.setBid1Volume(responseData[BID_1_VOLUME_INDEX]);
+      sinaStock.setBid1Price(responseData[BID_1_PRICE_INDEX]);
+      sinaStock.setBid2Volume(responseData[BID_2_VOLUME_INDEX]);
+      sinaStock.setBid2Price(responseData[BID_2_PRICE_INDEX]);
+      sinaStock.setBid3Volume(responseData[BID_3_VOLUME_INDEX]);
+      sinaStock.setBid3Price(responseData[BID_3_PRICE_INDEX]);
+      sinaStock.setBid4Volume(responseData[BID_4_VOLUME_INDEX]);
+      sinaStock.setBid4Price(responseData[BID_4_PRICE_INDEX]);
+      sinaStock.setBid5Volume(responseData[BID_5_VOLUME_INDEX]);
+      sinaStock.setBid5Price(responseData[BID_5_PRICE_INDEX]);
+      sinaStock.setAsk1Volume(responseData[ASK_1_VOLUME_INDEX]);
+      sinaStock.setAsk1Price(responseData[ASK_1_PRICE_INDEX]);
+      sinaStock.setAsk2Volume(responseData[ASK_2_VOLUME_INDEX]);
+      sinaStock.setAsk2Price(responseData[ASK_2_PRICE_INDEX]);
+      sinaStock.setAsk3Volume(responseData[ASK_3_VOLUME_INDEX]);
+      sinaStock.setAsk3Price(responseData[ASK_3_PRICE_INDEX]);
+      sinaStock.setAsk4Volume(responseData[ASK_4_VOLUME_INDEX]);
+      sinaStock.setAsk4Price(responseData[ASK_4_PRICE_INDEX]);
+      sinaStock.setAsk5Volume(responseData[ASK_5_VOLUME_INDEX]);
+      sinaStock.setAsk5Price(responseData[ASK_5_PRICE_INDEX]);
+      sinaStock.setDate(responseData[DATE_INDEX]);
+      sinaStock.setTime(responseData[TIME_INDEX]);
+
+      // Convert price string to float, then round to 2 digits.
+      //sinaStock.setN(String.format("$%.2f", Float.parseFloat(responseData[0])));
+
+    } catch (Exception e) {
+
+      Log.d("IOERROR", "Error occurred during generating data from http response:" + e.toString());
+    } return sinaStock;
   }
 
   // This method will retrieve the price chart from Yahoo and returns it as a drawable
@@ -117,5 +159,16 @@ public class SinaFinanceInfo {
     }
 
     return d;
+  }
+
+  public static String recover(String str)
+      throws Exception {
+    return new String(str.getBytes("GBK"), "UTF-8");
+  }
+
+  @Test
+  public void test() {
+    SinaStock sinaStock = SinaFinanceInfo.getStockData("601006");
+    System.out.print(sinaStock.toString());
   }
 }
---
+++ SimpleStocks/src/main/java/com/kevinchou/simplestocks/SinaStock.java
@@ -0,0 +1,355 @@
+package com.kevinchou.simplestocks;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ *
+ */
+public class SinaStock {
+  private String stockName;
+  private String openPrice;
+  private String preCosePrice;
+  private String curPrice;
+  private String dayHigh;
+  private String dayLow;
+  private String bid1;
+  private String ask1;
+  private String volume;
+  private String transactionAmount;
+  private String bid1Volume;
+  private String bid1Price;
+  private String bid2Volume;
+  private String bid2Price;
+  private String bid3Volume;
+  private String bid3Price;
+  private String bid4Volume;
+  private String bid4Price;
+  private String bid5Volume;
+  private String bid5Price;
+  private String ask1Volume;
+  private String ask1Price;
+  private String ask2Volume;
+  private String ask2Price;
+  private String ask3Volume;
+  private String ask3Price;
+  private String ask4Volume;
+  private String ask4Price;
+  private String ask5Volume;
+  private String ask5Price;
+  private String date;
+  private String time;
+
+  private int ticketNumber;
+  private Drawable priceChart;
+
+  public String getStockName() {
+    return stockName;
+  }
+
+  public void setStockName(String stockName) {
+    this.stockName = stockName;
+  }
+
+  public String getOpenPrice() {
+    return openPrice;
+  }
+
+  public void setOpenPrice(String openPrice) {
+    this.openPrice = openPrice;
+  }
+
+  public String getPreCosePrice() {
+    return preCosePrice;
+  }
+
+  public void setPreCosePrice(String preCosePrice) {
+    this.preCosePrice = preCosePrice;
+  }
+
+  public String getCurPrice() {
+    return curPrice;
+  }
+
+  public void setCurPrice(String curPrice) {
+    this.curPrice = curPrice;
+  }
+
+  public String getDayHigh() {
+    return dayHigh;
+  }
+
+  public void setDayHigh(String dayHigh) {
+    this.dayHigh = dayHigh;
+  }
+
+  public String getDayLow() {
+    return dayLow;
+  }
+
+  public void setDayLow(String dayLow) {
+    this.dayLow = dayLow;
+  }
+
+  public String getBid1() {
+    return bid1;
+  }
+
+  public void setBid1(String bid1) {
+    this.bid1 = bid1;
+  }
+
+  public String getAsk1() {
+    return ask1;
+  }
+
+  public void setAsk1(String ask1) {
+    this.ask1 = ask1;
+  }
+
+  public String getVolume() {
+    return volume;
+  }
+
+  public void setVolume(String volume) {
+    this.volume = volume;
+  }
+
+  public String getTransactionAmount() {
+    return transactionAmount;
+  }
+
+  public void setTransactionAmount(String transactionAmount) {
+    this.transactionAmount = transactionAmount;
+  }
+
+  public String getBid1Volume() {
+    return bid1Volume;
+  }
+
+  public void setBid1Volume(String bid1Volume) {
+    this.bid1Volume = bid1Volume;
+  }
+
+  public String getBid1Price() {
+    return bid1Price;
+  }
+
+  public void setBid1Price(String bid1Price) {
+    this.bid1Price = bid1Price;
+  }
+
+  public String getBid2Volume() {
+    return bid2Volume;
+  }
+
+  public void setBid2Volume(String bid2Volume) {
+    this.bid2Volume = bid2Volume;
+  }
+
+  public String getBid2Price() {
+    return bid2Price;
+  }
+
+  public void setBid2Price(String bid2Price) {
+    this.bid2Price = bid2Price;
+  }
+
+  public String getBid3Volume() {
+    return bid3Volume;
+  }
+
+  public void setBid3Volume(String bid3Volume) {
+    this.bid3Volume = bid3Volume;
+  }
+
+  public String getBid3Price() {
+    return bid3Price;
+  }
+
+  public void setBid3Price(String bid3Price) {
+    this.bid3Price = bid3Price;
+  }
+
+  public String getBid4Volume() {
+    return bid4Volume;
+  }
+
+  public void setBid4Volume(String bid4Volume) {
+    this.bid4Volume = bid4Volume;
+  }
+
+  public String getBid4Price() {
+    return bid4Price;
+  }
+
+  public void setBid4Price(String bid4Price) {
+    this.bid4Price = bid4Price;
+  }
+
+  public String getBid5Volume() {
+    return bid5Volume;
+  }
+
+  public void setBid5Volume(String bid5Volume) {
+    this.bid5Volume = bid5Volume;
+  }
+
+  public String getBid5Price() {
+    return bid5Price;
+  }
+
+  public void setBid5Price(String bid5Price) {
+    this.bid5Price = bid5Price;
+  }
+
+  public String getAsk1Volume() {
+    return ask1Volume;
+  }
+
+  public void setAsk1Volume(String ask1Volume) {
+    this.ask1Volume = ask1Volume;
+  }
+
+  public String getAsk1Price() {
+    return ask1Price;
+  }
+
+  public void setAsk1Price(String ask1Price) {
+    this.ask1Price = ask1Price;
+  }
+
+  public String getAsk2Volume() {
+    return ask2Volume;
+  }
+
+  public void setAsk2Volume(String ask2Volume) {
+    this.ask2Volume = ask2Volume;
+  }
+
+  public String getAsk2Price() {
+    return ask2Price;
+  }
+
+  public void setAsk2Price(String ask2Price) {
+    this.ask2Price = ask2Price;
+  }
+
+  public String getAsk3Volume() {
+    return ask3Volume;
+  }
+
+  public void setAsk3Volume(String ask3Volume) {
+    this.ask3Volume = ask3Volume;
+  }
+
+  public String getAsk3Price() {
+    return ask3Price;
+  }
+
+  public void setAsk3Price(String ask3Price) {
+    this.ask3Price = ask3Price;
+  }
+
+  public String getAsk4Volume() {
+    return ask4Volume;
+  }
+
+  public void setAsk4Volume(String ask4Volume) {
+    this.ask4Volume = ask4Volume;
+  }
+
+  public String getAsk4Price() {
+    return ask4Price;
+  }
+
+  public void setAsk4Price(String ask4Price) {
+    this.ask4Price = ask4Price;
+  }
+
+  public String getAsk5Volume() {
+    return ask5Volume;
+  }
+
+  public void setAsk5Volume(String ask5Volume) {
+    this.ask5Volume = ask5Volume;
+  }
+
+  public String getAsk5Price() {
+    return ask5Price;
+  }
+
+  public void setAsk5Price(String ask5Price) {
+    this.ask5Price = ask5Price;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public int getTicketNumber() {
+    return ticketNumber;
+  }
+
+  public void setTicketNumber(int ticketNumber) {
+    this.ticketNumber = ticketNumber;
+  }
+
+  public Drawable getPriceChart() {
+    return priceChart;
+  }
+
+  public void setPriceChart(Drawable priceChart) {
+    this.priceChart = priceChart;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb= new StringBuilder();
+    sb.append("Ticker number: " + ticketNumber + "\r\n");
+    sb.append("Name: " + stockName + "\r\n");
+    sb.append("Open price: " + openPrice + "\r\n");
+    sb.append("Previous day close price: " + preCosePrice + "\r\n");
+    sb.append("Current price: " + curPrice + "\r\n");
+    sb.append("Highest price in the day: " + dayHigh + "\r\n");
+    sb.append("Lowest price in the day: " + dayLow + "\r\n");
+    sb.append("Bid-1 price: " + bid1 + "\r\n");
+    sb.append("Ask-1 price: " + ask1 + "\r\n");
+    sb.append("Volume: " + volume + "\r\n");
+    sb.append("Transaction amount: " + transactionAmount + "\r\n");
+    sb.append("Bid-1 volume: " + bid1Volume + "\r\n");
+    sb.append("Bid-1 price: " + bid1Price + "\r\n");
+    sb.append("Bid-2 volume: " + bid2Volume + "\r\n");
+    sb.append("Bid-2 price: " + bid2Price + "\r\n");
+    sb.append("Bid-3 volume: " + bid3Volume + "\r\n");
+    sb.append("Bid-3 price: " + bid3Price + "\r\n");
+    sb.append("Bid-4 volume: " + bid4Volume + "\r\n");
+    sb.append("Bid-4 price: " + bid4Price + "\r\n");
+    sb.append("Bid-5 volume: " + bid5Volume + "\r\n");
+    sb.append("Bid-5 price: " + bid5Price + "\r\n");
+    sb.append("Ask-1 volume: " + ask1Volume + "\r\n");
+    sb.append("Ask-1 price: " + ask1Price + "\r\n");
+    sb.append("Ask-2 volume: " + ask2Volume + "\r\n");
+    sb.append("Ask-2 price: " + ask2Price + "\r\n");
+    sb.append("Ask-3 volume: " + ask3Volume + "\r\n");
+    sb.append("Ask-3 price: " + ask3Price + "\r\n");
+    sb.append("Ask-4 volume: " + ask4Volume + "\r\n");
+    sb.append("Ask-4 price: " + ask4Price + "\r\n");
+    sb.append("Ask-5 volume: " + ask5Volume + "\r\n");
+    sb.append("Ask-5 price: " + ask5Price + "\r\n");
+    sb.append("Date: " + date + "\r\n");
+    sb.append("Time: " + time + "\r\n");
+    return sb.toString();
+  }
+}
